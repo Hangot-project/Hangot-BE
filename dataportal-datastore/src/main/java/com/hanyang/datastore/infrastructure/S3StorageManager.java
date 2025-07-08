afc87b3ba0d8f12d20c293388d5da368461dfa8d
mintaek22 <biba99@naver.com>
2025-07-08 23:11:53 +0900

    fix: s3 조회 예외처리 추가

--- dataportal-datastore/src/main/java/com/hanyang/datastore/infrastructure/S3StorageManager.java
+++ dataportal-datastore/src/main/java/com/hanyang/datastore/infrastructure/S3StorageManager.java
@@ -1,13 +1,12 @@
 package com.hanyang.datastore.infrastructure;
 
+import com.hanyang.datastore.core.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 
@@ -17,28 +16,40 @@ public class S3StorageManager {
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+    
     private final S3Client s3Client;
-    private final static String folderName = "Resource";
+    private static final String FOLDER_NAME = "Resource";
 
-    public InputStream getFile(String datasetId){
+    public InputStream getFile(String datasetId) {
+        try {
+            String key = findFirstFileKey(datasetId);
+            
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
 
+            return s3Client.getObject(getObjectRequest);
+            
+        } catch (S3Exception e) {
+            throw new ResourceNotFoundException("S3에서 파일을 찾을 수 없습니다: " + datasetId);
+        }
+    }
+
+    private String findFirstFileKey(String datasetId) {
         ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
                 .bucket(bucket)
-                .prefix(folderName+"/"+datasetId+"/")
+                .prefix(FOLDER_NAME + "/" + datasetId + "/")
+                .maxKeys(1) // 첫 번째 파일만 필요
                 .build();
 
         ListObjectsV2Response response = s3Client.listObjectsV2(listObjectsRequest);
         List<S3Object> s3ObjectsList = response.contents();
 
-        String key = null;
-        for (S3Object object : s3ObjectsList) {
-            key =object.key();
+        if (s3ObjectsList.isEmpty()) {
+            throw new ResourceNotFoundException("데이터셋에 해당하는 파일이 없습니다: " + datasetId);
         }
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucket)
-                .key(key)
-                .build();
 
-       return s3Client.getObject(getObjectRequest);
+        return s3ObjectsList.get(0).key();
     }
 }
